1127c11a838ff9c8b59b203761d2e320551f55a3
Li Xiong <li.xiong@intelex.com>
2017-10-03 12:21:51 -0400

    Setup Environment

---
+++ client/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//      <div style={{textAlign: 'center'}}>
+//         <h1>Hello World</h1>
+//       </div>);
+//   }
+// }
+
+const App = function() {
+    return (
+        <div>
+            <h1>test</h1>
+        </div>
+    );
+}
+
+module.exports = App;
