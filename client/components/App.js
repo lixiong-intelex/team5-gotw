ad799755cd9028ae84fbab4af78805c72684c194
Li Xiong <li.xiong@intelex.com>
2017-11-08 17:20:39 -0500

    clean up to start blank

--- client/components/App.js
+++ client/components/App.js
@@ -1,15 +1,9 @@
 import React from 'react'
-import { BrowserRouter, Route, Router } from 'react-router-dom'
-import Routes from '../routes';
-import Navigation from './Navigation';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <Navigation></Navigation>
-        <Routes />
-      </div>
+      <h1>App</h1>
     )
   }
 }
