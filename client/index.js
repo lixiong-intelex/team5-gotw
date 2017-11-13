535ff6d03065bfc168cbbdabeaa702a3e7dad990
Brian Benwell <brian.benwell@intelex.com>
2017-11-13 15:15:59 -0500

    Add redux / react-redux, stubs for routes and redux store.

--- client/index.js
+++ client/index.js
@@ -4,5 +4,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
 import styles from './styles/styles.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import configureStore from './store/configureStore';
+import Routes from './routes'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routes = Routes;
+//const store = configureStore();
+
+ReactDOM.render(
+    <Router routes={routes}>
+        <App />
+    </Router>
+    , document.getElementById('root')
+);
---
+++ client/reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+// import {myClass} from './myClassReducer';
+
+const rootReducer = combineReducers({
+    // myClass,
+});
+
+export default rootReducer;
